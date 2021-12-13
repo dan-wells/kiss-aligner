704d071cd5fe4a6da92ce9dff2337b021ee5b6b8
Dan Wells <danwells396@gmail.com>
2021-12-13 22:14:13 +0000

    Change default KALDI_ROOT
    Just a bit neater

--- path.sh
+++ path.sh
@@ -1,5 +1,5 @@
 # Configure path for Kaldi binaries
-export KALDI_ROOT=$HOME/tools/kaldi
+export KALDI_ROOT=$HOME/kaldi
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
