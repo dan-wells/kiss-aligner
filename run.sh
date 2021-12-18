f4b3c92ef22e195b7b4ea7e876b5badabaf46db6
Dan Wells <danwells396@gmail.com>
2021-12-18 19:51:23 +0000

    Reformat run.sh

--- run.sh
+++ run.sh
@@ -46,6 +46,9 @@ Options:
 
 set -euo pipefail
 
+data=$workdir/data
+exp=$workdir/exp
+
 # TODO: some smart handling of input metadata filenames to name 
 # align/data/$part subdirectories
 #meta_base=${meta##*/}
@@ -67,20 +70,21 @@ fi
 if [ $stage -le 1 ]; then
   [ $exit_on_oov = true ] && warn_on_oov="--warn-on-oov" || warn_on_oov=""
   local/check_oov.py --workdir $workdir \
-    $workdir/data/lang/words.txt $workdir/data/train/text \
+    $data/lang/words.txt $data/train/text \
     $warn_on_oov || echo "Check OOV files: $workdir/oov_{words,utts}.txt" && exit 1
-  utils/prepare_lang.sh $workdir/data/local/dict \
-    ${oov%,*} $workdir/data/local/lang $workdir/data/lang
+  utils/prepare_lang.sh $data/local/dict \
+    ${oov%,*} $data/local/lang $data/lang
 fi
 
 if [ $stage -le 2 ]; then
   [ "$resample_method" == "kaldi" ] && mfcc_config=$workdir/conf/mfcc.conf
-  steps/make_mfcc.sh --cmd "$train_cmd" --nj $nj --mfcc-config $mfcc_config \
-    $workdir/data/train $workdir/data/train/mfcc $workdir/data/train/mfcc
-  steps/compute_cmvn_stats.sh $workdir/data/train \
-    $workdir/data/train/mfcc $workdir/data/train/mfcc
+  steps/make_mfcc.sh --cmd "$train_cmd" --nj $nj \
+    --mfcc-config $mfcc_config \
+    $data/train $data/train/mfcc $data/train/mfcc
+  steps/compute_cmvn_stats.sh \
+    $data/train $data/train/mfcc $data/train/mfcc
   # clear out any missing data
-  utils/fix_data_dir.sh $workdir/data/train
+  utils/fix_data_dir.sh $data/train
 fi
 
 if [ $stage -le 3 ]; then
@@ -89,72 +93,78 @@ if [ $stage -le 3 ]; then
   # should make it easier to align the data from a flat start (maybe be careful
   # about this in case there are repeated prompts across speakers).
   # TODO: Make these split sizes configurable (will fail if not enough utterances)
-  utils/validate_data_dir.sh $workdir/data/train
-  utils/subset_data_dir.sh --shortest $workdir/data/train 2000 $workdir/data/train_2kshort
-  utils/subset_data_dir.sh $workdir/data/train 5000 $workdir/data/train_5k
-  utils/subset_data_dir.sh $workdir/data/train 10000 $workdir/data/train_10k
+  utils/validate_data_dir.sh $data/train
+  utils/subset_data_dir.sh --shortest $data/train 2000 $data/train_2kshort
+  utils/subset_data_dir.sh $data/train 5000 $data/train_5k
+  utils/subset_data_dir.sh $data/train 10000 $data/train_10k
 fi
 
 if [ $stage -le 4 ]; then
   # train a monophone system on 2k short utts
-  steps/train_mono.sh --nj $nj --cmd "$train_cmd" --boost-silence $boost_silence \
-    $workdir/data/train_2kshort $workdir/data/lang $workdir/exp/mono
+  steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
+    --boost-silence $boost_silence \
+    $data/train_2kshort $data/lang $exp/mono
   # align next training subset
-  steps/align_si.sh --nj $nj --cmd "$train_cmd" --boost-silence $boost_silence \
-    $workdir/data/train_5k $workdir/data/lang $workdir/exp/mono $workdir/exp/mono_ali_5k
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+    --boost-silence $boost_silence \
+    $data/train_5k $data/lang $exp/mono $exp/mono_ali_5k
 fi
 
 if [ $stage -le 5 ]; then
   # train a first delta + delta-delta triphone system on a subset of 5000 utterances
-  steps/train_deltas.sh --cmd "$train_cmd" --boost-silence $boost_silence \
-    2000 10000 $workdir/data/train_5k $workdir/data/lang $workdir/exp/mono_ali_5k $workdir/exp/tri1
+  steps/train_deltas.sh --cmd "$train_cmd" \
+    --boost-silence $boost_silence \
+    2000 10000 \
+    $data/train_5k $data/lang $exp/mono_ali_5k $exp/tri1
   steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-    $workdir/data/train_10k $workdir/data/lang $workdir/exp/tri1 $workdir/exp/tri1_ali_10k
+    $data/train_10k $data/lang $exp/tri1 $exp/tri1_ali_10k
 fi
 
 if [ $stage -le 6 ]; then
   # train an LDA+MLLT system on 10k utts
   steps/train_lda_mllt.sh --cmd "$train_cmd" \
     --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
-    $workdir/data/train_10k $workdir/data/lang $workdir/exp/tri1_ali_10k $workdir/exp/tri2b
+    $data/train_10k $data/lang $exp/tri1_ali_10k $exp/tri2b
   # align a 10k utts subset using the tri2b model
-  steps/align_si.sh  --nj $nj --cmd "$train_cmd" --use-graphs true \
-    $workdir/data/train_10k $workdir/data/lang $workdir/exp/tri2b $workdir/exp/tri2b_ali_10k
+  steps/align_si.sh  --nj $nj --cmd "$train_cmd" \
+    --use-graphs true \
+    $data/train_10k $data/lang $exp/tri2b $exp/tri2b_ali_10k
 fi
 
 if [ $stage -le 7 ]; then
   # train tri3b, which is LDA+MLLT+SAT on 10k utts
-  steps/train_sat.sh --cmd "$train_cmd" 2500 15000 \
-    $workdir/data/train_10k $workdir/data/lang $workdir/exp/tri2b_ali_10k $workdir/exp/tri3b
+  steps/train_sat.sh --cmd "$train_cmd" \
+    2500 15000 \
+    $data/train_10k $data/lang $exp/tri2b_ali_10k $exp/tri3b
   # align the entire train_clean_100 subset using the tri3b model
   steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-    $workdir/data/train $workdir/data/lang \
-    $workdir/exp/tri3b $workdir/exp/tri3b_ali_train
+    $data/train $data/lang $exp/tri3b $exp/tri3b_ali_train
 fi
 
 if [ $stage -le 8 ]; then
   # train another LDA+MLLT+SAT system on the entire data set
-  steps/train_sat.sh  --cmd "$train_cmd" 4200 40000 \
-    $workdir/data/train $workdir/data/lang \
-    $workdir/exp/tri3b_ali_train $workdir/exp/tri4b
+  steps/train_sat.sh  --cmd "$train_cmd" \
+    4200 40000 \
+    $data/train $data/lang $exp/tri3b_ali_train $exp/tri4b
   # align train_clean_100 using the tri4b model
   steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-    $workdir/data/train $workdir/data/lang $workdir/exp/tri4b $workdir/exp/tri4b_ali_train
+    $data/train $data/lang $exp/tri4b $exp/tri4b_ali_train
   # check retried and failed utterances
-  local/check_alignments.sh $workdir/exp/tri4b_ali_train $workdir
+  local/check_alignments.sh $exp/tri4b_ali_train $workdir
 fi
 
 if [ $stage -le 9 ]; then
   # get word- and phone-level CTM files from final alignments
-  steps/get_train_ctm.sh --cmd "$train_cmd" --print-silence true \
-    $workdir/data/train $workdir/data/lang $workdir/exp/tri4b_ali_train
+  steps/get_train_ctm.sh --cmd "$train_cmd" \
+    --print-silence true \
+    $data/train $data/lang $exp/tri4b_ali_train
   local/get_phone_ctm.sh --cmd "$train_cmd" \
-    $workdir/data/lang $workdir/exp/tri4b_ali_train
+    $data/lang $exp/tri4b_ali_train
 fi
 
 if [ $stage -le 10 ]; then
   # split CTM files for final per-utterance outputs
   [ $strip_pos == true ] && strip_pos="--strip-pos" || strip_pos=""
-  local/split_ctm.py $strip_pos $workdir/exp/tri4b_ali_train/ctm $workdir/word
-  local/split_ctm.py $strip_pos $workdir/exp/tri4b_ali_train/ctm.phone $workdir/phone
+  local/split_ctm.py $strip_pos $exp/tri4b_ali_train/ctm $workdir/word
+  local/split_ctm.py $strip_pos $exp/tri4b_ali_train/ctm.phone $workdir/phone
 fi
