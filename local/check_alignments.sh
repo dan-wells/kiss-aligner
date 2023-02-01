78bbdf1476b9ad20c997b51f78c775c8cfff2d93
Dan Wells <danwells396@gmail.com>
2023-02-01 17:51:21 +0000

    Speed up checking successful/retried/failed alignments

--- local/check_alignments.sh
+++ local/check_alignments.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ -f ./path.sh ] && . ./path.sh
+
 ali=$1
 workdir=$2
 data=$3
@@ -18,7 +20,9 @@ grep -h "WARNING" $ali/log/align_pass2.*.log > $workdir/warnings
 grep -Po "Retrying utterance \K.+ (?=with)" $workdir/warnings > $workdir/retried_utts
 grep -Po "Did not successfully decode file \K.+(?=,)" $workdir/warnings > $workdir/failed_utts
 
-grep -f $workdir/failed_utts -v $workdir/retried_utts | grep -f - $data/text > $retried
+# note retried but not failed alignments (also included in final text)
+utils/filter_scp.pl $workdir/retried_utts $data/text | \
+  utils/filter_scp.pl --exclude $workdir/failed_utts > $retried
 num_retried=$(cat $retried | wc -l)
 if [ $num_retried -gt 0 ]; then
   echo "Aligned $num_retried utterances on second attempt using wider beam: $retried"
@@ -26,12 +30,16 @@ else
   rm $retried
 fi
 
-grep -f $workdir/failed_utts $data/text > $failed
+# note failed alignments
+utils/filter_scp.pl $workdir/failed_utts $data/text > $failed
 num_failed=$(cat $failed | wc -l)
 if [ $num_failed -gt 0 ]; then
   echo "Failed to align $num_failed utterances: $failed"
 else
   rm $failed
 fi
+
+# filter failed alignments from final text
+utils/filter_scp.pl --exclude $workdir/failed_utts $data/text > $workdir/text
 
 rm $workdir/{warnings,failed_utts,retried_utts}
