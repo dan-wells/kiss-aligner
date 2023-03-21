2a5714e99a76255f3d5db42ada34ee13498238dc
Dan Wells <danwells396@gmail.com>
2023-03-21 13:41:19 +0000

    Robustness/fixes

--- egs/learngaelic_litir/run.sh
+++ egs/learngaelic_litir/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 LITIR=egs/learngaelic_litir
 
 # Grab long audio files and transcripts
@@ -11,7 +13,7 @@ mkdir $LITIR/data/wav_long
 for ogg in $LITIR/data/ogg_long/*.ogg; do
   wav=${ogg##*/}
   wav=${wav/ogg/wav}
-  sox $ogg -b 16 -c 1 -r 16k $LITIR/data/wav_long/$wav
+  sox -G $ogg -b 16 -c 1 -r 16k $LITIR/data/wav_long/$wav
 done
 
 # Split long audio files on silences at least 1.5s long
@@ -21,12 +23,12 @@ python3 $LITIR/local/split_audios.py --nj 8 \
 
 # Split long text transcripts on phrase-final punctuation
 # NB. before text normalisation because closing smart quotes can indicate sentence ends
-mkdir $LITIR/data/text_chunked
+mkdir -p $LITIR/data/text_chunked
 python3 $LITIR/local/split_and_number_sentences.py \
   $LITIR/data/text_long $LITIR/data/text_chunked
 
 # Approximately align audio and text chunks based on cumulative durations
-mkdir -p align_dtw/data/train
+mkdir -p $LITIR/align_dtw/data/train
 python3 $LITIR/local/dtw_audio_and_text_lens.py \
   $LITIR/data/wav_chunked $LITIR/data/text_chunked $LITIR/align_dtw/data/train
 
@@ -44,7 +46,7 @@ cp $LITIR/align_dtw/data/train/spk2utt $LITIR/align_dtw/data/train/utt2spk
 
 # Run initial alignment
 # TODO: adjust nj
-./run.sh --workdir egs/learngaelic_litir/align_dtw \
+./run.sh --workdir $LITIR/align_dtw \
   --nj 8 --stage 1 --mfcc-config conf/mfcc.conf \
   --beam 10 --retry-beam 0 --careful true
 
@@ -57,7 +59,7 @@ mkdir -p $LITIR/segment_utts/data/long
 for text in $LITIR/data/text_long/*.txt; do
   utt=${text##*/}
   utt=${utt%.txt}
-  oneline=$(paste -sd' ' $txt)
+  oneline=$(paste -sd' ' $text)
   echo "$utt $oneline" >> $LITIR/segment_utts/data/long/text_long_raw
 done
 python3 $LITIR/local/normalize_text.py \
